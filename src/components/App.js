2d14cf3a4d633ef31bed8fa6fe59dca9752d3a0c
Sumit Singh <sumit.sssingh.singh@gmail.com>
2020-08-19 19:12:48 +0530

    added name credits

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import ListAppointments from './ListAppointments';
 import SearchAppointments from './SearchAppointments';
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <ListAppointments />
-              <SearchAppointments />
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      myName: 'Sumit Kumar Singh',
+      authorName: 'Ray Villalobos'
+    }
+  }
+
+  render() {
+    const nameStyle = {
+      float: 'right',
+    }
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <div style={nameStyle}>
+                  Practice build by : <b>{this.state.myName}</b><br />
+                  Original Credits: <b>{this.state.authorName}</b>
+                </div>
+
+                <AddAppointments />
+                <ListAppointments />
+                <SearchAppointments />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
